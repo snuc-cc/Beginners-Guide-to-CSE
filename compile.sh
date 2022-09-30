6b2b48ed56c60d0c5362ea649e17f4797b833448
Vishal Kalathil <kalathilvishal1@gmail.com>
2022-09-30 10:38:39 +0530

    Finishing up script

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
-touch Book.tex
-# Insert template
-pandoc -f gfm  -t latex --top-level-division=section src/*.md >> Book.tex
+HEADER=src/template/header.txt
+FOOTER=src/template/footer.txt
+FINAL_FILE=Book
+touch $FINAL_FILE.tex
+cat "$HEADER" >> $FINAL_FILE.tex
+pandoc -f gfm  -t latex --top-level-division=section src/*.md >> $FINAL_FILE.tex
+cat "$FOOTER" >> $FINAL_FILE.tex
+pdflatex $FINAL_FILE.tex
+rm $FINAL_FILE.log $FINAL_FILE.aux $FINAL_FILE.out $FINAL_FILE.tex
 echo "Done"
