abd24815bdcdb3f641ea56d7f011155c60ae3696
Vishal Kalathil <kalathilvishal1@gmail.com>
2022-10-08 22:59:03 +0530

    Minor Fixes
    Changed title page

--- compile.sh
+++ compile.sh
@@ -7,5 +7,6 @@ cat "$HEADER" >> $FINAL_FILE.tex
 pandoc -f gfm  -t latex --top-level-division=chapter src/*.md >> $FINAL_FILE.tex
 cat "$FOOTER" >> $FINAL_FILE.tex
 pdflatex $FINAL_FILE.tex
+pdflatex $FINAL_FILE.tex # Double compilation for the Table of Contents
 rm $FINAL_FILE.log $FINAL_FILE.aux $FINAL_FILE.out $FINAL_FILE.tex
 echo "Done"
