ac9cd3842ee37805ef9191da1591b5f739fb17a2
Vishal Kalathil <kalathilvishal1@gmail.com>
2022-10-08 21:56:19 +0530

    Change document type to book and top level division to chapter
    makes the PDF look ✨pretty✨

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-HEADER=src/template/header.txt
-FOOTER=src/template/footer.txt
+HEADER=src/template/header.tex
+FOOTER=src/template/footer.tex
 FINAL_FILE=Book
 touch $FINAL_FILE.tex
 cat "$HEADER" >> $FINAL_FILE.tex
-pandoc -f gfm  -t latex --top-level-division=section src/*.md >> $FINAL_FILE.tex
+pandoc -f gfm  -t latex --top-level-division=chapter src/*.md >> $FINAL_FILE.tex
 cat "$FOOTER" >> $FINAL_FILE.tex
 pdflatex $FINAL_FILE.tex
 rm $FINAL_FILE.log $FINAL_FILE.aux $FINAL_FILE.out $FINAL_FILE.tex
