a1ddabb4a51014813c630ec9b9b7aac562d798d4
Vishal Kalathil <kalathilvishal1@gmail.com>
2022-11-28 22:18:57 +0530

    Minor changes

--- compile.sh
+++ compile.sh
@@ -6,7 +6,7 @@ mkdir pdf
 cd pdf
 touch $FINAL_FILE.tex
 cat "$HEADER" >> $FINAL_FILE.tex
-pandoc -f gfm  -t latex --top-level-division=chapter src/*.md >> $FINAL_FILE.tex
+pandoc -f gfm  -t latex --top-level-division=chapter ../src/*.md >> $FINAL_FILE.tex
 cat "$FOOTER" >> $FINAL_FILE.tex
 pdflatex $FINAL_FILE.tex
 pdflatex $FINAL_FILE.tex # Double compilation for the Table of Contents
